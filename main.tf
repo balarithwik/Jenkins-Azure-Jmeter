8ef41ad33d4c550a396e7c813667e7b08af81032
Bala Rithwik <rithwik10122000@gmai.com>
2026-02-15 17:35:12 +0530

    Added JMeter test, HTML pages, and Jenkins pipeline

--- main.tf
+++ main.tf
@@ -1,34 +1,56 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.64.0"
+      version = "= 3.64.0"
     }
   }
 }
 
+# --------------------------------------------------
+# Provider
+# --------------------------------------------------
 provider "azurerm" {
   features {}
-  subscription_id            = "c3e1d504-633c-4752-b82f-7af3da9f2fc1"
-  tenant_id                  = "7b8e0a9a-8ff5-4d1e-a54f-a0f4d0664488"
   skip_provider_registration = true
 }
 
-# ----------------------------
-# Resource Group
-# ----------------------------
-resource "azurerm_resource_group" "rg" {
-  name     = "demo-rg"
-  location = "West US 2"
+# --------------------------------------------------
+# Variables
+# --------------------------------------------------
+variable "location" {
+  default = "West US 2"
 }
 
-# ----------------------------
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "ssh_public_key_path" {
+  default = "C:/Users/Bala/.ssh/id_rsa.pub"
+}
+
+variable "ssh_private_key_path" {
+  default = "C:/Users/Bala/.ssh/id_rsa"
+}
+
+# --------------------------------------------------
+# Resource Group
+# --------------------------------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "demo-rg"
+  location = var.location
+}
+
+# --------------------------------------------------
 # Networking
-# ----------------------------
+# --------------------------------------------------
 resource "azurerm_virtual_network" "vnet" {
   name                = "demo-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
@@ -41,28 +63,15 @@ resource "azurerm_subnet" "subnet" {
 
 resource "azurerm_public_ip" "vm_ip" {
   name                = "vm-public-ip"
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "demo-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "primary"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_ip.id
-  }
-}
-
 resource "azurerm_network_security_group" "nsg" {
   name                = "demo-nsg"
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
@@ -90,26 +99,41 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+resource "azurerm_network_interface" "nic" {
+  name                = "demo-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "primary"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_ip.id
+  }
+}
+
 resource "azurerm_network_interface_security_group_association" "assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-# ----------------------------
-# Virtual Machine
-# ----------------------------
+# --------------------------------------------------
+# Linux VM
+# --------------------------------------------------
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "demo-vm"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   size                = "Standard_D4s_v3"
-  admin_username      = "azureuser"
+  admin_username      = var.admin_username
 
-  network_interface_ids = [azurerm_network_interface.nic.id]
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
 
   admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("C:/Users/Bala/.ssh/id_rsa.pub")
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
   }
 
   os_disk {
@@ -124,32 +148,31 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
+  depends_on = [
+    azurerm_network_interface_security_group_association.assoc
+  ]
+
   provisioner "remote-exec" {
     inline = [
       "set -e",
 
       "sudo apt-get update -y",
 
-      # Disable swap
       "sudo swapoff -a",
       "sudo sed -i '/ swap / s/^/#/' /etc/fstab",
 
-      # Kernel modules
       "sudo modprobe overlay",
       "sudo modprobe br_netfilter",
 
-      # Sysctl
       "sudo tee /etc/sysctl.d/k8s.conf <<EOF\nnet.bridge.bridge-nf-call-iptables=1\nnet.bridge.bridge-nf-call-ip6tables=1\nnet.ipv4.ip_forward=1\nEOF",
       "sudo sysctl --system",
 
-      # Containerd
       "sudo apt-get install -y containerd",
       "sudo mkdir -p /etc/containerd",
       "sudo containerd config default | sudo tee /etc/containerd/config.toml",
       "sudo systemctl restart containerd",
       "sudo systemctl enable containerd",
 
-      # Kubernetes packages
       "sudo apt-get install -y apt-transport-https ca-certificates curl",
       "sudo mkdir -p /etc/apt/keyrings",
       "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg",
@@ -159,36 +182,32 @@ resource "azurerm_linux_virtual_machine" "vm" {
       "sudo apt-get install -y kubelet kubeadm kubectl",
       "sudo apt-mark hold kubelet kubeadm kubectl",
 
-      # Kubernetes init (NO WAITING)
       "sudo kubeadm init --pod-network-cidr=10.244.0.0/16 || true",
 
-      # kubeconfig
-      "mkdir -p /home/azureuser/.kube",
-      "sudo cp /etc/kubernetes/admin.conf /home/azureuser/.kube/config",
-      "sudo chown azureuser:azureuser /home/azureuser/.kube/config",
+      "mkdir -p /home/${var.admin_username}/.kube",
+      "sudo cp /etc/kubernetes/admin.conf /home/${var.admin_username}/.kube/config",
+      "sudo chown ${var.admin_username}:${var.admin_username} /home/${var.admin_username}/.kube/config",
 
-      # Flannel (apply only, no wait)
       "kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml",
 
-      # Allow scheduling
       "kubectl taint nodes --all node-role.kubernetes.io/control-plane- || true",
 
-      "echo 'Terraform bootstrap completed'"
+      "echo 'Terraform Kubernetes bootstrap completed'"
     ]
 
     connection {
       type        = "ssh"
-      user        = "azureuser"
-      private_key = file("C:/Users/Bala/.ssh/id_rsa")
+      user        = var.admin_username
+      private_key = file(var.ssh_private_key_path)
       host        = azurerm_public_ip.vm_ip.ip_address
       timeout     = "45m"
     }
   }
 }
 
-# ----------------------------
-# Output
-# ----------------------------
+# --------------------------------------------------
+# Outputs
+# --------------------------------------------------
 output "vm_public_ip" {
   value = azurerm_public_ip.vm_ip.ip_address
 }
